501ffced1dd946c625b0d9a146626e7822c8a9c8
PhongSeow <seowphong@hotmail.com>
2021-07-04 10:31:42 +0000

    Add GetFileAndDirListLibDemo.1.0.1

---
+++ Release/GetFileAndDirListLibDemo/SampleDemo.md
@@ -0,0 +1,20 @@
+```
+VB.net
+Module Program
+    Sub Main(args As String())
+        Dim oConsoleDemo As New GetFileAndDirListLib.ConsoleDemo
+        oConsoleDemo.Main()
+    End Sub
+End Module
+
+C#
+class Program
+{
+    static void Main(string[] args)
+    {
+        GetFileAndDirListLib.ConsoleDemo oConsoleDemo = new GetFileAndDirListLib.ConsoleDemo() ;
+        oConsoleDemo.Main();
+    }
+}
+
+```
---
+++ Src/DotNet/GetFileAndDirListLibDemo/GetFileAndDirListLibDemo/ConsoleDemo.vb
@@ -0,0 +1,45 @@
+﻿Imports GetFileAndDirListLib
+
+Public Class ConsoleDemo
+    Public GetFileAndDirListApp As GetFileAndDirListApp
+    Public RootDirPath As String
+
+    Public Sub Main()
+        Do While True
+            Console.WriteLine("*******************")
+            Console.WriteLine("Main menu")
+            Console.WriteLine("*******************")
+            Console.WriteLine("Press Q to Exit")
+            Console.WriteLine("Press A to Scan Folders Dir and Files")
+            Console.WriteLine("*******************")
+            Select Case Console.ReadKey().Key
+                Case ConsoleKey.Q
+                    Exit Do
+                Case ConsoleKey.A
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("Scan Folders Dir and Files")
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("Enter the starting folder for the scan")
+                    Me.RootDirPath = Console.ReadLine
+                    Me.GetFileAndDirListApp = New GetFileAndDirListApp(Me.RootDirPath)
+                    Console.WriteLine("RootDirPath=" & Me.RootDirPath)
+                    If Me.GetFileAndDirListApp.LastErr <> "" Then
+                        Console.WriteLine(Me.GetFileAndDirListApp.LastErr)
+                    Else
+                        Console.WriteLine("Start")
+                        Me.GetFileAndDirListApp.Start()
+                        If Me.GetFileAndDirListApp.LastErr <> "" Then
+                            Console.WriteLine(Me.GetFileAndDirListApp.LastErr)
+                        Else
+                            Console.WriteLine("Scanning succeeded, please view DirList.txt and FileList.txt.")
+                        End If
+                    End If
+            End Select
+        Loop
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+
+End Class
