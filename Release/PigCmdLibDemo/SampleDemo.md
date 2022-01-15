25858d9a135f65a2a8999bf499b7a095fe9e9b1c
seowphong <seowphong@hotmail.com>
2022-01-15 21:47:55 +0800

    Add PigCmdLibDemo.1.0.2

---
+++ Release/PigCmdLibDemo/SampleDemo.md
@@ -0,0 +1,20 @@
+```
+VB.net
+Module Program
+    Sub Main(args As String())
+        Dim oConsoleDemo As New PigCmdLibDemo.ConsoleDemo
+        oConsoleDemo.Main()
+    End Sub
+End Module
+
+C#
+class Program
+{
+    static void Main(string[] args)
+    {
+        PigCmdLibDemo.ConsoleDemo oConsoleDemo = new PigCmdLibDemo.ConsoleDemo() ;
+        oConsoleDemo.Main();
+    }
+}
+
+```
---
+++ Src/DotNet/PigCmdLibDemo/PigCmdLibDemo/ConsoleDemo.vb
@@ -0,0 +1,40 @@
+﻿Imports PigCmdLib
+
+Public Class ConsoleDemo
+    Public CmdOrFilePath As String
+    Public CmdPara As String
+    Public PigCmdApp As New PigCmdApp
+    Public PID As Long
+    Public Line As String
+    Public Sub Main()
+        Do While True
+            Console.WriteLine("*******************")
+            Console.WriteLine("Main menu")
+            Console.WriteLine("*******************")
+            Console.WriteLine("Press Q to Exit")
+            Console.WriteLine("Press A to HideShell")
+            Console.WriteLine("*******************")
+            Select Case Console.ReadKey().Key
+                Case ConsoleKey.Q
+                    Exit Do
+                Case ConsoleKey.A
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("HideShell")
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("CmdFilePath=" & Me.CmdOrFilePath)
+                    Me.Line = Console.ReadLine
+                    If Me.Line <> "" Then
+                        Me.CmdOrFilePath = Me.Line
+                    End If
+                    Me.PID = PigCmdApp.HideShell(Me.CmdOrFilePath)
+                    Console.WriteLine("LastErr=" & Me.PigCmdApp.LastErr)
+                    Console.WriteLine("PID=" & Me.PID)
+            End Select
+        Loop
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+
+End Class
