494d3e6e724de6247cb14583ce389f381dd1ac79
seowphong <seowphong@hotmail.com>
2022-01-15 19:02:32 +0800

    Add PigCmdLib.1.0.22

---
+++ Src/DotNet/PigCmdLib/PigCmdLib/PigCmdApp.vb
@@ -0,0 +1,103 @@
+﻿'**********************************
+'* Name: CmdShell
+'* Author: Seow Phong
+'* License: Copyright (c) 2022 Seow Phong, For more details, see the MIT LICENSE file included with this distribution.
+'* Describe: 调用操作系统命令的应用|Application of calling operating system commands
+'* Home Url: https://www.seowphong.com or https://en.seowphong.com
+'* Version: 1.0
+'* Create Time: 15/1/2022
+'**********************************
+Imports PigToolsLiteLib
+Public Class PigCmdApp
+    Inherits PigBaseMini
+    Private Const CLS_VERSION As String = "1.0.8"
+    Public LinuxShPath As String = "/bin/sh"
+    Public WindowsCmdPath As String
+    Public Sub New()
+        MyBase.New(CLS_VERSION)
+    End Sub
+    ''' <summary>
+    ''' 类似VB6的隐藏窗口的Shell命令|Shell command of hidden window similar to VB6
+    ''' </summary>
+    ''' <param name="CmdFilePath">命令文件路径</param>
+    ''' <returns>执行文件的进程号|Shell command of hidden window similar to VB6</returns>
+    Public Function HideShell(CmdFilePath As String) As Long
+        If Me.IsWindows = True Then
+            Return Me.mWinHideShell(CmdFilePath)
+        Else
+            Return Me.mLinuxHideShell(CmdFilePath)
+        End If
+    End Function
+
+    Private Function mWinHideShell(CmdFilePath As String) As Long
+        Dim LOG As New PigStepLog("mWinHideShell")
+        Try
+            LOG.StepName = "New ProcessStartInfo"
+            Dim moProcessStartInfo As New ProcessStartInfo(CmdFilePath)
+            With moProcessStartInfo
+                .UseShellExecute = True
+                .CreateNoWindow = True
+            End With
+            LOG.StepName = "Process.Start"
+            Dim oProcess As Process = Process.Start(moProcessStartInfo)
+            mWinHideShell = oProcess.Id
+            oProcess = Nothing
+            moProcessStartInfo = Nothing
+            Me.ClearErr()
+        Catch ex As Exception
+            If Me.IsDebug = True Then
+                LOG.AddStepNameInf(CmdFilePath)
+            End If
+            Me.SetSubErrInf(LOG.SubName, LOG.StepName, ex)
+            Return -1
+        End Try
+    End Function
+
+    Private Function mLinuxHideShell(CmdFilePath As String) As Long
+        Dim LOG As New PigStepLog("mLinuxHideShell")
+        Try
+            LOG.StepName = "New ProcessStartInfo"
+            Dim moProcessStartInfo As New ProcessStartInfo(Me.LinuxShPath)
+            With moProcessStartInfo
+                .UseShellExecute = False
+                .CreateNoWindow = True
+                .RedirectStandardInput = True
+            End With
+            LOG.StepName = "Process.Start"
+            Dim oProcess As Process = Process.Start(moProcessStartInfo)
+            LOG.StepName = "Process.StandardInput"
+            oProcess.StandardInput.WriteLine(CmdFilePath)
+            mLinuxHideShell = oProcess.Id
+            oProcess = Nothing
+            moProcessStartInfo = Nothing
+            Me.ClearErr()
+        Catch ex As Exception
+            If Me.IsDebug = True Then
+                LOG.AddStepNameInf(CmdFilePath)
+            End If
+            Me.SetSubErrInf(LOG.SubName, LOG.StepName, ex)
+            Return -1
+        End Try
+    End Function
+
+    Private mstrStandardOutput As String = ""
+    Public Property StandardOutput As String
+        Get
+            Return mstrStandardOutput
+        End Get
+        Friend Set(value As String)
+            mstrStandardOutput = value
+        End Set
+    End Property
+
+    Private mstrStandardError As String = ""
+    Public Property StandardError As String
+        Get
+            Return mstrStandardError
+        End Get
+        Friend Set(value As String)
+            mstrStandardError = value
+        End Set
+    End Property
+
+End Class
