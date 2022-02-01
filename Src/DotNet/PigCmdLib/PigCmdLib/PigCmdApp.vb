aa92cd4b74a91cad8760d27ad9b6d0f1331f7ee3
seowphong <seowphong@hotmail.com>
2022-02-01 18:07:01 +0800

    Add PigCmdLib.1.2.28

--- Src/DotNet/PigCmdLib/PigCmdLib/PigCmdApp.vb
+++ Src/DotNet/PigCmdLib/PigCmdLib/PigCmdApp.vb
@@ -4,22 +4,107 @@
 '* License: Copyright (c) 2022 Seow Phong, For more details, see the MIT LICENSE file included with this distribution.
 '* Describe: 调用操作系统命令的应用|Application of calling operating system commands
 '* Home Url: https://www.seowphong.com or https://en.seowphong.com
-'* Version: 1.0
+'* Version: 1.2
 '* Create Time: 15/1/2022
+'*1.1  31/1/2022   Add CallFile, modify mWinHideShell,mLinuxHideShell
+'*1.2  1/2/2022   Add CmdShell, modify CallFile
 '**********************************
 Imports PigToolsLiteLib
+Imports System.IO
 Public Class PigCmdApp
     Inherits PigBaseMini
-    Private Const CLS_VERSION As String = "1.0.8"
+    Private Const CLS_VERSION As String = "1.2.8"
     Public LinuxShPath As String = "/bin/sh"
     Public WindowsCmdPath As String
+    Private moPigFunc As New PigFunc
     Public Sub New()
         MyBase.New(CLS_VERSION)
+        If Me.IsWindows = True Then
+            Me.WindowsCmdPath = moPigFunc.GetEnvVar("windir") & "\System32\cmd.exe"
+        End If
     End Sub
+
+    Private mintCmdWaitForExitTime As Integer = 10
+    Public Property CmdWaitForExitTime As Long
+        Get
+            Return mintCmdWaitForExitTime
+        End Get
+        Set(value As Long)
+            If value < 0 Then value = 10
+            mintCmdWaitForExitTime = value
+        End Set
+    End Property
+
+    Private mlngPID As Long
+    Public Property PID As Long
+        Get
+            Return mlngPID
+        End Get
+        Friend Set(value As Long)
+            mlngPID = value
+        End Set
+    End Property
+
+
+    Private mstrStandardOutput As String = ""
+    Public Property StandardOutput As String
+        Get
+            Return mstrStandardOutput
+        End Get
+        Friend Set(value As String)
+            mstrStandardOutput = value
+        End Set
+    End Property
+
+    Private mstrStandardError As String = ""
+    Public Property StandardError As String
+        Get
+            Return mstrStandardError
+        End Get
+        Friend Set(value As String)
+            mstrStandardError = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' 执行操作系统命令|Execute operating system commands
+    ''' </summary>
+    ''' <param name="Cmd">命令语句|Command statement</param>
+    ''' <returns></returns>
+    Public Function CmdShell(Cmd As String) As String
+        Dim LOG As New PigStepLog("CmdShell")
+        Try
+            Dim strShellPath As String
+            If Me.IsWindows = True Then
+                strShellPath = Me.WindowsCmdPath
+            Else
+                strShellPath = Me.LinuxShPath
+            End If
+            Dim strCmd As String
+            Cmd = Replace(Cmd, """", """""")
+            If Me.IsWindows = True Then
+                strCmd = " /c "
+            Else
+                strCmd = " -c "
+            End If
+            strCmd &= """" & Cmd & """"
+            LOG.StepName = "CallFile"
+            LOG.Ret = Me.CallFile(strShellPath, strCmd)
+            If LOG.Ret <> "OK" Then Throw New Exception(LOG.Ret)
+            Return "OK"
+        Catch ex As Exception
+            If Me.IsDebug = True Then
+                LOG.AddStepNameInf(Cmd)
+            End If
+            Return Me.GetSubErrInf(LOG.SubName, LOG.StepName, ex)
+        End Try
+    End Function
+
+
     ''' <summary>
     ''' 类似VB6的隐藏窗口的Shell命令|Shell command of hidden window similar to VB6
     ''' </summary>
-    ''' <param name="CmdFilePath">命令文件路径</param>
+    ''' <param name="CmdFilePath">命令文件路径|Command file path</param>
     ''' <returns>执行文件的进程号|Shell command of hidden window similar to VB6</returns>
     Public Function HideShell(CmdFilePath As String) As Long
         If Me.IsWindows = True Then
@@ -41,6 +126,7 @@ Public Class PigCmdApp
             LOG.StepName = "Process.Start"
             Dim oProcess As Process = Process.Start(moProcessStartInfo)
             mWinHideShell = oProcess.Id
+            Me.PID = oProcess.Id
             oProcess = Nothing
             moProcessStartInfo = Nothing
             Me.ClearErr()
@@ -68,6 +154,7 @@ Public Class PigCmdApp
             LOG.StepName = "Process.StandardInput"
             oProcess.StandardInput.WriteLine(CmdFilePath)
             mLinuxHideShell = oProcess.Id
+            Me.PID = oProcess.Id
             oProcess = Nothing
             moProcessStartInfo = Nothing
             Me.ClearErr()
@@ -80,24 +167,54 @@ Public Class PigCmdApp
         End Try
     End Function
 
-    Private mstrStandardOutput As String = ""
-    Public Property StandardOutput As String
-        Get
-            Return mstrStandardOutput
-        End Get
-        Friend Set(value As String)
-            mstrStandardOutput = value
-        End Set
-    End Property
+    ''' <summary>
+    ''' 调用文件|Call file
+    ''' </summary>
+    ''' <param name="FilePath">调用文件的路径|Path to the calling file</param>
+    ''' <param name="Para">调用文件的参数|Call file parameters</param>
+    ''' <returns></returns>
+    Public Function CallFile(FilePath As String, Para As String) As String
+        Dim LOG As New PigStepLog("CallFile")
+        Try
+            LOG.StepName = "New ProcessStartInfo"
+            Dim moProcessStartInfo As New ProcessStartInfo(FilePath)
+            With moProcessStartInfo
+                .UseShellExecute = False
+                .CreateNoWindow = True
+                .RedirectStandardError = True
+                .RedirectStandardOutput = True
+                .Arguments = Para
+            End With
+            LOG.StepName = "Process.Start"
+            Dim oProcess As Process = Process.Start(moProcessStartInfo)
+            Me.PID = oProcess.Id
+            LOG.StepName = "Process.StandardOutput"
+            Dim srStandardOutput As StreamReader = oProcess.StandardOutput
+            LOG.StepName = "StandardOutput.ReadToEnd"
+            Me.StandardOutput = srStandardOutput.ReadToEnd
+            LOG.StepName = "Process.StandardOutput.WaitForExit"
+            oProcess.WaitForExit(Me.CmdWaitForExitTime)
+            srStandardOutput = Nothing
+            LOG.StepName = "Process.StandardError"
+            Dim srStandardError As StreamReader = oProcess.StandardError
+            LOG.StepName = "srStandardError.ReadToEnd"
+            Me.StandardError = srStandardError.ReadToEnd
+            LOG.StepName = "Process.StandardError.WaitForExit"
+            oProcess.WaitForExit(Me.CmdWaitForExitTime)
+            srStandardError = Nothing
+            LOG.StepName = "Process.Close"
+            oProcess.Close()
+            oProcess = Nothing
+            moProcessStartInfo = Nothing
+            Return "OK"
+        Catch ex As Exception
+            If Me.IsDebug = True Then
+                LOG.AddStepNameInf(FilePath)
+                LOG.AddStepNameInf(Para)
+            End If
+            Return Me.GetSubErrInf(LOG.SubName, LOG.StepName, ex)
+        End Try
+    End Function
 
-    Private mstrStandardError As String = ""
-    Public Property StandardError As String
-        Get
-            Return mstrStandardError
-        End Get
-        Friend Set(value As String)
-            mstrStandardError = value
-        End Set
-    End Property
 
 End Class
--- Src/DotNet/PigCmdLibDemo/PigCmdLibDemo/ConsoleDemo.vb
+++ Src/DotNet/PigCmdLibDemo/PigCmdLibDemo/ConsoleDemo.vb
@@ -1,13 +1,28 @@
-﻿Imports PigCmdLib
+﻿'**********************************
+'* Name: ConsoleDemo
+'* Author: Seow Phong
+'* License: Copyright (c) 2022 Seow Phong, For more details, see the MIT LICENSE file included with this distribution.
+'* Describe: 
+'* Home Url: https://www.seowphong.com or https://en.seowphong.com
+'* Version: 1.2.1
+'* Create Time: 15/1/2022
+'* 1.1    31/1/2022   Add CallFile
+'* 1.1    1/3/2022   Add CmdShell
+'************************************
+
+Imports PigCmdLib
 
 Public Class ConsoleDemo
     Public CmdOrFilePath As String
     Public CmdPara As String
     Public PigCmdApp As New PigCmdApp
     Public PID As Long
+    Public Cmd As String
+    Public Para As String
     Public Line As String
     Public PigConsole As New PigConsole
     Public Pwd As String
+    Public Ret As String
     Public Sub Main()
         Do While True
             Console.WriteLine("*******************")
@@ -15,7 +30,9 @@ Public Class ConsoleDemo
             Console.WriteLine("*******************")
             Console.WriteLine("Press Q to Exit")
             Console.WriteLine("Press A to HideShell")
-            Console.WriteLine("Press B to GetPwdStr")
+            Console.WriteLine("Press B to CallFile")
+            Console.WriteLine("Press C to CmdShell")
+            Console.WriteLine("Press D to GetPwdStr")
             Console.WriteLine("*******************")
             Console.CursorVisible = False
             Select Case Console.ReadKey(True).Key
@@ -25,7 +42,7 @@ Public Class ConsoleDemo
                     Console.WriteLine("*******************")
                     Console.WriteLine("HideShell")
                     Console.WriteLine("*******************")
-                    Console.WriteLine("CmdFilePath=" & Me.CmdOrFilePath)
+                    Console.WriteLine("Cmd=" & Me.Cmd)
                     Console.CursorVisible = True
                     Me.Line = Console.ReadLine
                     If Me.Line <> "" Then
@@ -35,6 +52,45 @@ Public Class ConsoleDemo
                     Console.WriteLine("LastErr=" & Me.PigCmdApp.LastErr)
                     Console.WriteLine("PID=" & Me.PID)
                 Case ConsoleKey.B
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("CallFile")
+                    Console.WriteLine("*******************")
+                    Console.CursorVisible = True
+                    Console.WriteLine("FilePath=" & Me.CmdOrFilePath)
+                    Me.Line = Console.ReadLine
+                    If Me.Line <> "" Then
+                        Me.CmdOrFilePath = Me.Line
+                    End If
+                    Console.WriteLine("Para=" & Me.Para)
+                    Me.Line = Console.ReadLine
+                    If Me.Line <> "" Then
+                        Me.Para = Me.Line
+                    End If
+                    Me.Ret = PigCmdApp.CallFile(Me.CmdOrFilePath, Me.Para)
+                    Console.WriteLine("CallFile=" & Me.Ret)
+                    If Me.Ret = "OK" Then
+                        Console.WriteLine("PID=" & Me.PigCmdApp.PID)
+                        Console.WriteLine("StandardOutput=" & Me.PigCmdApp.StandardOutput)
+                        Console.WriteLine("StandardError=" & Me.PigCmdApp.StandardError)
+                    End If
+                Case ConsoleKey.C
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("CmdShell")
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("Cmd=" & Me.Cmd)
+                    Console.CursorVisible = True
+                    Me.Line = Console.ReadLine
+                    If Me.Line <> "" Then
+                        Me.Cmd = Me.Line
+                    End If
+                    Me.Ret = PigCmdApp.CmdShell(Me.Cmd)
+                    Console.WriteLine("CmdShell=" & Me.Ret)
+                    If Me.Ret = "OK" Then
+                        Console.WriteLine("PID=" & Me.PigCmdApp.PID)
+                        Console.WriteLine("StandardOutput=" & Me.PigCmdApp.StandardOutput)
+                        Console.WriteLine("StandardError=" & Me.PigCmdApp.StandardError)
+                    End If
+                Case ConsoleKey.D
                     Console.WriteLine("*******************")
                     Console.WriteLine("GetPwdStr")
                     Console.WriteLine("*******************")
