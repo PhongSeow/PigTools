4b1032af0f1fea679dfde1e9727815a5ad0a2cf9
seowphong <seowphong@hotmail.com>
2022-04-02 22:44:35 +0800

    Add PigWebCtrlLib

---
+++ Src/DotNet/PigWebCtrlLib/PigWebCtrlLib/PigWebLogic.vb
@@ -0,0 +1,17 @@
+﻿'**********************************
+'* Name: 豚豚WebLogic|PigProc
+'* Author: Seow Phong
+'* License: Copyright (c) 2022 Seow Phong, For more details, see the MIT LICENSE file included with this distribution.
+'* Describe: WebLogic 处理|WebLogic processing
+'* Home Url: https://www.seowphong.com or https://en.seowphong.com
+'* Version: 1.0
+'* Create Time: 3/4/2022
+'**********************************
+Public Class PigWebLogic
+    Inherits PigBaseMini
+    Private Const CLS_VERSION As String = "1.0.0"
+    Public Sub New(ProcessID As Integer)
+        MyBase.New(CLS_VERSION)
+    End Sub
+
+End Class
