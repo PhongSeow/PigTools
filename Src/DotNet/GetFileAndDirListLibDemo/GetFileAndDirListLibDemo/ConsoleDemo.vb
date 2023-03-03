f11627b74188cb12025a330b0aaad50eccf025fd
seowphong <seowphong@hotmail.com>
2023-03-03 09:45:27 +0800

    Add GetFileAndDirListLibDemo.1.5.2.

--- Src/DotNet/GetFileAndDirListLibDemo/GetFileAndDirListLibDemo/ConsoleDemo.vb
+++ Src/DotNet/GetFileAndDirListLibDemo/GetFileAndDirListLibDemo/ConsoleDemo.vb
@@ -1,8 +1,15 @@
 ﻿Imports GetFileAndDirListLib
+Imports PigCmdLib
+Imports PigToolsLiteLib
+
 
 Public Class ConsoleDemo
-    Public GetFileAndDirListApp As GetFileAndDirListApp
+    Public WithEvents GetFileAndDirListApp As GetFileAndDirListApp
+    Public PigConsole As New PigConsole
     Public RootDirPath As String
+    Public LogFilePath As String
+    Public Ret As String
+    Public PigFunc As New PigFunc
 
     Public Sub Main()
         Do While True
@@ -10,35 +17,66 @@ Public Class ConsoleDemo
             Console.WriteLine("Main menu")
             Console.WriteLine("*******************")
             Console.WriteLine("Press Q to Exit")
-            Console.WriteLine("Press A to Scan Folders Dir and Files")
-            '            Console.WriteLine("Press B to Test Linux")
+            Console.WriteLine("Press A to New")
+            Console.WriteLine("Press B to Start")
+            Console.WriteLine("Press C to Show Status")
             Console.WriteLine("*******************")
             Select Case Console.ReadKey().Key
                 Case ConsoleKey.Q
                     Exit Do
                 Case ConsoleKey.A
                     Console.WriteLine("*******************")
-                    Console.WriteLine("Scan Folders Dir and Files")
+                    Console.WriteLine("New")
                     Console.WriteLine("*******************")
-                    Console.WriteLine("Enter the starting folder for the scan")
-                    Me.RootDirPath = Console.ReadLine
-                    Me.GetFileAndDirListApp = New GetFileAndDirListApp(Me.RootDirPath)
+                    Me.Ret = Me.PigConsole.GetLine("Enter the starting folder for the scan", Me.RootDirPath)
+                    If Me.Ret <> "OK" Then Console.WriteLine(Me.Ret)
+                    Me.Ret = Me.PigConsole.GetLine("Enter the log file path", Me.LogFilePath)
+                    If Me.Ret <> "OK" Then Console.WriteLine(Me.Ret)
+                    Me.GetFileAndDirListApp = New GetFileAndDirListApp(Me.RootDirPath, Me.LogFilePath)
                     Console.WriteLine("RootDirPath=" & Me.RootDirPath)
-                    '                    Console.WriteLine("LinuxTest=" & Me.GetFileAndDirListApp.LinuxTest)
                     If Me.GetFileAndDirListApp.LastErr <> "" Then
                         Console.WriteLine(Me.GetFileAndDirListApp.LastErr)
+                    End If
+                Case ConsoleKey.B
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("Start")
+                    Console.WriteLine("*******************")
+                    If Me.GetFileAndDirListApp Is Nothing Then
+                        Console.WriteLine("GetFileAndDirListApp Is Nothing")
                     Else
+                        Dim bolIsFull As Boolean = False
+                        bolIsFull = Me.PigConsole.IsYesOrNo("Is full PigMD5")
                         Console.WriteLine("Start" & Me.GetFileAndDirListApp.DirListPath)
-                        Me.GetFileAndDirListApp.Start()
+                        Me.GetFileAndDirListApp.Start(bolIsFull)
                         If Me.GetFileAndDirListApp.LastErr <> "" Then
                             Console.WriteLine(Me.GetFileAndDirListApp.LastErr)
                         Else
-                            Console.WriteLine("Scanning succeeded, please view DirList.txt and FileList.txt.")
+                            Console.WriteLine("Start scanning.")
                         End If
                     End If
-                    'Case ConsoleKey.B
-                    '    Me.GetFileAndDirListApp = New GetFileAndDirListApp
-                    '    Console.WriteLine("LinuxTest=" & Me.GetFileAndDirListApp.LinuxTest)
+                Case ConsoleKey.C
+                    Console.WriteLine("*******************")
+                    Console.WriteLine("Show Status")
+                    Console.WriteLine("*******************")
+                    If Me.GetFileAndDirListApp Is Nothing Then
+                        Console.WriteLine("GetFileAndDirListApp Is Nothing")
+                    Else
+                        With Me.GetFileAndDirListApp
+                            .RefStatus()
+                            Console.WriteLine("RunStatus=" & .RunStatus.ToString)
+                            Console.WriteLine("StartTime=" & .StartTime)
+                            Console.WriteLine("TimeoutTime=" & .TimeoutTime)
+                            Console.WriteLine("EndTime=" & .EndTime)
+                            Console.WriteLine("UseSeconds=" & .UseSeconds)
+                            Console.WriteLine("ScanFolders=" & .ScanFolders)
+                            Console.WriteLine("ScanFiles=" & .ScanFiles)
+                            Console.WriteLine("IsAbsolutePath=" & .IsAbsolutePath)
+                            Console.WriteLine("IsFullPigMD5=" & .IsFullPigMD5)
+                            Console.WriteLine("CurrProcThreadID=" & .CurrProcThreadID)
+                            Console.WriteLine("RootDirPath=" & .RootDirPath)
+                            Console.WriteLine("LogFilePath=" & .LogFilePath)
+                        End With
+                    End If
             End Select
         Loop
     End Sub
@@ -47,4 +85,11 @@ Public Class ConsoleDemo
         MyBase.Finalize()
     End Sub
 
+    Private Sub GetFileAndDirListApp_ScanFail(ErrInf As String) Handles GetFileAndDirListApp.ScanFail
+        Console.WriteLine("GetFileAndDirListApp_ScanFail:" & ErrInf)
+    End Sub
+
+    Private Sub GetFileAndDirListApp_ScanOK() Handles GetFileAndDirListApp.ScanOK
+        Console.WriteLine("GetFileAndDirListApp_ScanOK")
+    End Sub
 End Class
